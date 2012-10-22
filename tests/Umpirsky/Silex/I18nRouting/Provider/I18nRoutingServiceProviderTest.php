bc83d12aa9ef98d275e5944d0becceda9931bbbd
umpirsky <umpirsky@gmail.com>
2012-10-22 10:20:00 +0200

    More tests.

--- tests/Umpirsky/Silex/I18nRouting/Provider/I18nRoutingServiceProviderTest.php
+++ tests/Umpirsky/Silex/I18nRouting/Provider/I18nRoutingServiceProviderTest.php
@@ -13,6 +13,7 @@ namespace Umpirsky\Silex\I18nRouting;
 
 use Silex\Application;
 use Silex\Provider\TranslationServiceProvider;
+use Symfony\Component\HttpFoundation\Request;
 use Umpirsky\Silex\I18nRouting\Provider\I18nRoutingServiceProvider;
 
 /**
@@ -25,20 +26,24 @@ class I18nRoutingServiceProviderTest extends \PHPUnit_Framework_TestCase
     protected function setUp()
     {
         $this->app = new Application();
-        $this->app->register(new TranslationServiceProvider());
-        $this->app['translator.domains'] = array('routes' => array('/route', '/ruta'));
-    }
 
-    public function testRegister()
-    {
+        $this->app->register(new TranslationServiceProvider());
         $this->app->register(new I18nRoutingServiceProvider());
 
+        $this->app['locale'] = 'sr_Latn';
+        $this->app['translator.domains'] = array('routes' => array('sr_Latn' => array('/route' => '/ruta')));
+    }
+
+    public function testRouteClass()
+    {
         $this->assertSame('Umpirsky\Silex\I18nRouting\Route', $this->app['route_class']);
     }
 
     public function testI18nRoutes()
     {
-        $this->app->register(new I18nRoutingServiceProvider());
-        $this->app->get('/foo', function() {});
+        $this->app->get('/route', function () {});
+
+        $this->assertEquals(200, $this->app->handle(Request::create('/ruta'))->getStatusCode());
+        $this->assertEquals(404, $this->app->handle(Request::create('/route'))->getStatusCode());
     }
 }
